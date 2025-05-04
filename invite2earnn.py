f8835be7cbbc48938c73fb71d1309121d43025ed
Mohammedalilgrh <164587973+Mohammedalilgrh@users.noreply.github.com>
2025-05-04 15:23:10 +0300

    invite2earnn.py

--- invite2earnn.py
+++ invite2earnn.py
@@ -8,8 +8,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 # Bot settings
 API_ID = 21706160
-API_HASH = '548b91f0e7cd2e44bbee05190620d9f4'
-BOT_TOKEN = '7551982212:AAHSgM4JuGnOBBzafGqGFZhY1-gwVo7g4nY'
+API_HASH = "548b91f0e7cd2e44bbee05190620d9f4"
+BOT_TOKEN = "7551982212:AAHpBiA097R5WIi-rHnNA-Bc6GbniFpOoVg"
 CHANNEL_USERNAME = "@invite2earnn"
 ORDER_CHANNEL = "@invite2orders"
 
